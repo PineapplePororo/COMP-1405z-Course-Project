7be847f7ccd63fb815050368333a384342631ed9
Kayeon <kayeonalvina@gmail.com>
2022-10-18 14:16:39 -0400

    crawler start final

--- crawler.py
+++ crawler.py
@@ -1,14 +1,38 @@
 import webdev
 
+dict = {}
+queue = []
+
 def crawl(seed):
+    help_crawl(seed)
+
+def help_crawl(url):
     pass
 
+def queueHandling(url):
 
-def queueHandling():
-    pass
+    global dict
+    global queue
+
+    if url not in dict:
+        
+        # make a new key with a value of 1
+        dict[url] = 1
+
+
+        # pass it to help_crawl
+
+
+
+
+
+
+    
+	
+
     
 
-print("everyone loves kdramas")
+# main
 # print(webdev.read_url("http://people.scs.carleton.ca/~davidmckenney/tinyfruits/N-0.html"))
 
 
