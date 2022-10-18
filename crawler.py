533d2e3e985f1a942bdae8b3bfd5bfd8a2c313bd
Kayeon <kayeonalvina@gmail.com>
2022-10-18 13:13:00 -0400

    Update crawler.py
    start crawl

--- crawler.py
+++ crawler.py
@@ -1,35 +1,13 @@
-import myqueue
 import webdev
 
 def crawl(seed):
-
-    #resets all existing data 
-    #performs webcrawl starting at the seed url; parses all pages that cna be reached through linsnks form the see URL
-    # gneeratis all data required for other parts of the project and saves that data to files. 
-
-    urlQueue = []
-
-    currURL = ""
-    nextURL = myqueue.dequeue(seed)
-
-    while(len(myqueue.isempty(urlQueue)) == False):
-        
-        currURL = nextURL      
-
-        #adding the text to a words array by splitting the url read by each empty space/newline
-        page = webdev.read_url(currURL.split())
-
-        #now we have to go throught the page contents 
-        #separate them based on tags (p tag or a tag to get text or links respectively)
-        #get the words and put them in a array or file or smthn
-        #get the links and enqueue them
-
-        nextURL = myqueue.dequeue(urlQueue)
+    pass
 
 
-    return "amongus hehe"
+def queueHandling():
+    pass
 
 
-print(webdev.read_url("http://people.scs.carleton.ca/~davidmckenney/tinyfruits/N-0.html"))
+# print(webdev.read_url("http://people.scs.carleton.ca/~davidmckenney/tinyfruits/N-0.html"))
 
 
