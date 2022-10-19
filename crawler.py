32959e773f458b24c6aa4ff22d2b439e982d0bb9
Kayeon <kayeonalvina@gmail.com>
2022-10-18 22:32:22 -0400

    crawl - almost done
    need to create a folder inside a file
    finish deleting files function

--- crawler.py
+++ crawler.py
@@ -1,6 +1,6 @@
+import abc
 import webdev
-
-
+import os
 
 def crawl(seed):
     # keeps track of the pages visited (used for runtime efficiency)
@@ -11,8 +11,13 @@ def crawl(seed):
     count = 0
 
     url = seed 
-    
+
     while (len(queue) != 0):
+
+        currentFile = abc
+        
+        # create folder and files for the url
+        wordFile, urlFile = createFolderFiles(currentFile)
 
         # extract pp
         # extract url
@@ -30,15 +35,39 @@ def crawl(seed):
             # increment count for all succeed crawls
             count += 1
 
-
-
-
-
-
-
     return count 
 
 
+# fuction to create the necessary folder and files
+def createFolderFiles(currentFile):
+
+    # if os.path.exists("crawl"):
+    #     print("folder exists")
+    # else:
+    #     # if there isn't, make a new folder with a name as the url
+    #     os.makedirs("crawl")
+
+    # create a directory for the url
+    # check if the folder with the same name exists
+    if os.path.exists(currentFile):
+        print("folder exists")
+    else:
+        # if there isn't, make a new folder with a name as the url
+        os.makedirs(currentFile)
+
+    # create files within the created folder
+    if os.path.isdir(currentFile):
+        # create two files: words for storing words and url for storing urls
+        wordPath = os.path.join(currentFile, "words")
+        urlPath = os.path.join(currentFile, "urls")
+
+    return wordPath, urlPath
+
+
+
+    
+
+    
 
 
 
