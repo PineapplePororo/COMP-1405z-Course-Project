7df37d5f4e79e43e666088d6ea2ea5f973dcf804
Kayeon <kayeonalvina@gmail.com>
2022-10-18 22:51:34 -0400

    crawler - almost almost done
    except for deleting files

--- crawler.py
+++ crawler.py
@@ -5,10 +5,14 @@ import os
 def crawl(seed):
     # keeps track of the pages visited (used for runtime efficiency)
     dict = {}
-    # 
+    # keeps track of urls that have been yet to be parsed through
     queue = []
     # keeps track of the number of total pages found
     count = 0
+
+    # check if the main folder that will contain all the data is made
+    # if not, create
+    createFolder()
 
     url = seed 
 
@@ -37,26 +41,30 @@ def crawl(seed):
 
     return count 
 
+# checks if the main folder that will have all the folders from crawl is made 
+def createFolder():
+    # create a folder that will contain all the folders from the crawl
+    if os.path.exists("crawl"):
+        print("folder exists")
+    else:
+        # if there isn't, make a new folder 
+        os.makedirs("crawl")
+
 
 # fuction to create the necessary folder and files
+# returns the word file path and url file path so that it could be used in crawl(seed)
 def createFolderFiles(currentFile):
-
-    # if os.path.exists("crawl"):
-    #     print("folder exists")
-    # else:
-    #     # if there isn't, make a new folder with a name as the url
-    #     os.makedirs("crawl")
 
     # create a directory for the url
     # check if the folder with the same name exists
-    if os.path.exists(currentFile):
-        print("folder exists")
+    if os.path.exists(os.path.join('crawl', currentFile)):
+        print("currentFile exists in crawl")
     else:
-        # if there isn't, make a new folder with a name as the url
-        os.makedirs(currentFile)
+        # if there isn't, make a new folder with a name same as the url
+        os.makedirs(os.path.join('crawl', currentFile))
 
     # create files within the created folder
-    if os.path.isdir(currentFile):
+    if os.path.isdir(os.path.join('crawl', currentFile)):
         # create two files: words for storing words and url for storing urls
         wordPath = os.path.join(currentFile, "words")
         urlPath = os.path.join(currentFile, "urls")
