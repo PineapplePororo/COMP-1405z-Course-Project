7df0e275d2ffcdfa0ec083b14e183e8c0cd4ce36
Kayeon <kayeonalvina@gmail.com>
2022-10-19 00:04:49 -0400

    crawl - working manageFolder()

--- crawler.py
+++ crawler.py
@@ -7,16 +7,24 @@ def manageFolder():
     # create a folder that will contain all the folders from the crawl
     if os.path.exists("crawl"):
         # if it's created already, check if there are files in the folder
+
+        # create a list of folders/files in the crawl folder
         dirs = os.listdir("crawl")
 
         # loop for all the subfolders in the crawl folder
         for dir in dirs:
+            # store the path of the subfolder
+            subfolderPath = os.path.join("crawl", dir)
+
+            # make a list of all files in the subfolder
+            files = os.listdir(subfolderPath)
+
             # loop for all the files in subfolder
-            for file in dir:
+            for file in files:
                 # remove every file
-                os.remove(os.path.join(dir, file))
+                os.remove(os.path.join(subfolderPath, file))
             # remove the subfolder
-            os.rmdir(dir)
+            os.rmdir(subfolderPath)
     else:
         # if there isn't, make a new folder 
         os.makedirs("crawl")
--- test.py
+++ test.py
@@ -1,14 +1,29 @@
-from tkinter import S
+import os
 
+def manageFolder():
+    # create a folder that will contain all the folders from the crawl
+    if os.path.exists("crawl"):
+        # if it's created already, check if there are files in the folder
 
-s1 = "<p>Hello my name is \n alvina Han<p/>"
-s = "<p>Hello<p/><p><p/>"
+        # create a list of folders/files in the crawl folder
+        dirs = os.listdir("crawl")
 
-# new = string.replace("<p>", "").replace("<p/>", "")
+        # loop for all the subfolders in the crawl folder
+        for dir in dirs:
+            # store the path of the subfolder
+            subfolderPath = os.path.join("crawl", dir)
 
-# print(new.split(" "))
+            # make a list of all files in the subfolder
+            files = os.listdir(subfolderPath)
 
-end = s.find("<p/>") 
-start = s.find("<p>")+3
+            # loop for all the files in subfolder
+            for file in files:
+                # remove every file
+                os.remove(os.path.join(subfolderPath, file))
+            # remove the subfolder
+            os.rmdir(subfolderPath)
+    else:
+        # if there isn't, make a new folder 
+        os.makedirs("crawl")
 
-print(s[start:end])
+manageFolder()
