deda8306bca2cd61a8bebd03b16fbfb2cb8a5d0b
omarkshah <omarkshah2@gmail.com>
2022-10-19 11:45:04 -0400

    updtated createlists

--- crawler.py
+++ crawler.py
@@ -32,31 +32,47 @@ def manageFolder():
 
 # fuction to create the necessary folder and files
 # returns the word file path and url file path so that it could be used in crawl(seed)
-def createFiles(currentFile):
+def createFiles(fileName):
 
     # check for any #\/:*?"<>|" and replace them with spaces as they aren't allowed to be in a folder
-    currentFile = currentFile.replace("\\", " ").replace('/', " ").replace(':', " ").replace('*', " ").replace('?', " ").replace('\"', " ").replace('<', " ").replace('>', " ").replace('|', " ")
+    #currentFile = currentFile.replace("\\", " ").replace('/', " ").replace(':', " ").replace('*', " ").replace('?', " ").replace('\"', " ").replace('<', " ").replace('>', " ").replace('|', " ")
 
     # store path for the subfolder of the current file
-    filePath = os.path.join('crawl', currentFile)
+    wordFilePath = os.path.join('crawl', "words")
+    urlFilePath = os.path.join('crawl', "urls")
 
     # create a directory for the url
     # check if the folder with the same name exists
-    if os.path.exists(filePath):
+    if os.path.exists(wordFilePath):
+        pass
+        #print("currentFile exists in crawl")
+    else:
+        # if there isn't, make a new folder with a name same as the url
+        os.makedirs(wordFilePath)
+    
+    # create a directory for the url
+    # check if the folder with the same name exists
+    if os.path.exists(urlFilePath):
         print("currentFile exists in crawl")
     else:
         # if there isn't, make a new folder with a name same as the url
-        os.makedirs(filePath)
+        os.makedirs(urlFilePath)
 
     # create files within the created folder
-    if os.path.isdir(filePath):
+    if os.path.isdir(wordFilePath):
         # create two files: words for storing words and url for storing urls
-        wordPath = os.path.join(filePath, "words")
-        urlPath = os.path.join(filePath, "urls")
+        wordPath = os.path.join(wordFilePath, fileName)
+        urlPath = os.path.join(wordFilePath, fileName)
 
         # create an empty word file
         if not os.path.exists(wordPath):
             open(wordPath, "w").close()
+
+    # create files within the created folder
+    if os.path.isdir(urlFilePath):
+        # create two files: words for storing words and url for storing urls
+        wordPath = os.path.join(urlFilePath, fileName)
+        urlPath = os.path.join(urlFilePath, fileName)
 
         # create an empty url file
         if not os.path.exists(urlPath):
@@ -76,8 +92,10 @@ def crawl(seed):
     # check if the main folder that will store all the information is made and empty
     manageFolder()
 
+    counter = 1
     while (len(queue) != 0):
 
+        #print(counter) 
         url = queue.pop()
 
         #finding the abosolute url by finding the last "/" and saving its index
@@ -92,8 +110,11 @@ def crawl(seed):
         absoluteUrl = url[0:end]
 
         # create folder and files for the url
-        wordFile, urlFile = createFiles(url[7:])
-        # page = webdev.read_url(url)
+        if(url not in dict): # creating the file if it has not yet been looked at
+            wordFile, urlFile = createFiles(str(counter))
+            counter += 1
+
+        page = webdev.read_url(url)
         
         words = ""
         urls = ""
@@ -103,7 +124,7 @@ def crawl(seed):
             start = page.find("<p>") + 3
             end = page.find("</p>")
 
-            words += page[start:end].split()
+            words += page[start:end]
 
             page = page.replace("<p>", "", 1)
             page = page.replace("</p>", "", 1)
@@ -122,35 +143,48 @@ def crawl(seed):
         words = words.split()
         urls = urls.split()
 
-        fileOut = open(wordFile, "r")
+        fileOut = open(wordFile, "w")
 
         for word in words:
-            fileOut.write(word)
+            fileOut.write(word  +"\n")
         
         fileOut.close()
 
-        fileOut = open(urlFile, "r")
+        fileOut = open(urlFile, "w")
 
         for url in urls:
             if(url[0] == "."):
-                fileOut.write(absoluteUrl + url[1:len(url) - 1])
+                fileOut.write(absoluteUrl + url[1:len(url)])
+                 # adding to queue 
+                # check if the url isn't in queue 
+                if url not in dict:
+                
+                    # make a new key of the url
+                    dict[url] = 0
+
+                    # add to end of queue
+                    queue.append(absoluteUrl + url[1:len(url)])
+
+                    # increment count for all succeed crawls
+                    count += 1
             else:
                 fileOut.write(url)
+                # adding to queue 
+                # check if the url isn't in queue 
+                if url not in dict:
+                
+                    # make a new key of the url
+                    dict[url] = 0
+
+                    # add to end of queue
+                    queue.append(url)
+
+                    # increment count for all succeed crawls
+                    count += 1
                 
 
-        # adding to queue 
-        # check if the url isn't in queue 
-        if url not in dict:
-        
-            # make a new key of the url
-            dict[url] = 0
-
-            # add to end of queue
-            queue.append(url)
-
-            # increment count for all succeed crawls
-            count += 1
+       
 
     return count 
 
-print(crawl("https://people.scs.carleton.ca/~davidmckenney/tinyfruits/N-0.html"))  
+print("THE COUNT IS: "  + crawl("https://people.scs.carleton.ca/~davidmckenney/tinyfruits/N-0.html"))  
