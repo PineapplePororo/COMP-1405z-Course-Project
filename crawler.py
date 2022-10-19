c03ccee7b3b520d6dce625fdb09eac2c64c17834
omarkshah <omarkshah2@gmail.com>
2022-10-18 22:30:34 -0400

    Added all the extraction code

--- crawler.py
+++ crawler.py
@@ -1,7 +1,5 @@
 import webdev
 
-
-
 def crawl(seed):
     # keeps track of the pages visited (used for runtime efficiency)
     dict = {}
@@ -14,8 +12,37 @@ def crawl(seed):
     
     while (len(queue) != 0):
 
-        # extract pp
-        # extract url
+        page = webdev.read_url(url)
+
+        # if there is pp extract pp
+        # if there is url extract url
+        
+        words = ""
+        urls = ""
+
+        while page.find("<p>") != -1:
+
+            start = page.find("<p>") + 3
+            end = page.find("</p>")
+
+            words += page[start:end].split()
+
+            page = page.replace("<p>", "", 1)
+            page = page.replace("</p>", "", 1)
+
+        while page.find("</a>") != -1:
+            
+            start = page.find('<a href="') + 9
+            end = page.find('.html">') + 5
+
+            urls += page[start:end] + " "
+
+            page = page.replace('href="', "", 1)
+            page = page.replace('.html">', "", 1)
+            page = page.replace('</a>', "", 1)
+
+        words = words.split()
+        urls = urls.split()
 
         # adding to queue 
         # check if the url isn't in queue 
@@ -29,19 +56,10 @@ def crawl(seed):
 
             # increment count for all succeed crawls
             count += 1
-
-
-
-
-
-
-
     return count 
 
 
 
 
 
-# print(webdev.read_url("http://people.scs.carleton.ca/~davidmckenney/tinyfruits/N-0.html"))
-
-
+print(webdev.read_url("http://people.scs.carleton.ca/~davidmckenney/tinyfruits/N-0.html"))  
