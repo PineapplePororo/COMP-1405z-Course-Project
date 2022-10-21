13887f946980bc4d97b40b926f85738afc84524b
omarkshah <omarkshah2@gmail.com>
2022-10-21 00:10:36 -0400

    Modified the crawl to extract more info
    Added tf and twf but cant get incoming to work

--- crawler.py
+++ crawler.py
@@ -55,6 +55,11 @@ def crawl(seed):
     # keeps track of the number of total pages found & folder name of the specific url
     count = 1
 
+    #total word frequency
+    twf = {}
+    tf = {}
+    incoming = {}
+
     # check if the main folder that will store all the information is made and empty
     manageFolder()
 
@@ -85,12 +90,14 @@ def crawl(seed):
         # create folder and files for the url
         urlFile = createFiles(str(dict[url]))
 
-
         page = webdev.read_url(url)
         
         words = ""
         urls = ""
 
+        uniqueWords = {}
+
+        # Extracting all words by finding the start and end p-tags and splicing the strings using those
         while page.find("<p>") != -1:
 
             start = page.find("<p>") + 3
@@ -101,6 +108,28 @@ def crawl(seed):
             page = page.replace("<p>", "", 1)
             page = page.replace("</p>", "", 1)
 
+        words = words.split()
+
+        # Getting the count for each word in the url and storing as a variable
+        for word in words:
+            if word in twf:
+                twf[word] += 1
+            else:
+                twf[word] = 0
+
+            if word in tf:
+                uniqueWords[word] += 1
+            else:
+                uniqueWords[word] = 1
+        
+        #Getting the frequency of each word and storing it in the uniquewords dictionary
+        for word in uniqueWords:
+            uniqueWords[word] /= len(words) 
+        
+          #Adding the uniques wrods of the current url into the tf dictionary
+        tf[dict[url].split("_")[0]] = uniqueWords
+
+        # Extracting links by finding the starting portion of the a-tag and the end of the link and using those indexes to splice
         while page.find("</a>") != -1:
             
             start = page.find('<a href="') + 9
@@ -112,18 +141,11 @@ def crawl(seed):
             page = page.replace('.html">', "", 1)
             page = page.replace('</a>', "", 1)
 
-        words = words.split()
-        urls = urls.split()
+        urls = urls.split()          
 
-        fileOut = open(wordFile, "w")
 
-        for word in words:
-            fileOut.write(word + "\n")
-        
-        fileOut.close()
-
+        #Writing all the urls to the url files
         fileOut = open(urlFile, "w")
-
         for url in urls:
             if(url[0] == "."):
                 url = absoluteUrl + url[1:len(url)]
@@ -144,7 +166,24 @@ def crawl(seed):
                 queue.append(url)
 
         fileOut.close()
-        
+
+        #THIS IS THE CODE FOR INCOMING
+        # Getting incoming links for all links in teh crawl
+        for link in urls:
+            if(link in incoming):
+                #appending the url into a list (value) inside the incoming dictionary
+                incoming[dict[str(absoluteUrl + link[1:])].split("_")[0]].append(url)
+            else:
+                #if the incoming list does not already exist, creating it then appending the url into it
+                incoming[dict[str(absoluteUrl + link[1:])].split("_")[0]] = []
+                incoming[dict[str(absoluteUrl + link[1:])].split("_")[0]].append(url)  
+
+    print("\n\n")
+    print(twf)
+    print("\n\n")
+    print(tf)
+    print("\n\n")
+    print(incoming)
 
     # dump to all dictioinaries to files using json
     with open(os.path.join('crawl', "0_dict.json"), 'w') as outfile:
@@ -165,7 +204,7 @@ def crawl(seed):
     return count 
 
 
-# crawl("http://people.scs.carleton.ca/~davidmckenney/tinyfruits/N-0.html")
+crawl("http://people.scs.carleton.ca/~davidmckenney/tinyfruits/N-0.html")
 
 # def time():
 #     import time
