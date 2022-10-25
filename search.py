f942038c6118e86b192861ee31c1737fd4e87b1b
Kayeon <kayeonalvina@gmail.com>
2022-10-25 16:15:46 -0400

    commenting

--- search.py
+++ search.py
@@ -9,6 +9,7 @@ reverseDict = json.load(open(os.path.join('crawl', "0_reverseDict.json"), "r"))
 # return a list of the top 10 ranked search results
 def search(phrase, boost):
 
+    # PART 1: VECTOR SPACE MODEL
     # store words in a list
     words = phrase.split(" ")
 
@@ -53,8 +54,7 @@ def search(phrase, boost):
         # tf-idf value for query
         query.append(math.log(1+((sameWord[words[i]])/total), 2)*searchdata.get_idf(words[i]))
 
-
-    # COSINE SIMILARITY CODE
+    # PART 2: COSINE SIMILARITY
     cosSimilarity = []
 
     # left denominator calculation (same throughout the calculation)
